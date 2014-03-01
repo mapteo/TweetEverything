3d82b1a303aefcb38c048ec5ec281e7bc91e9ac7
mapteo <matteo.zobbi@gmail.com>
2014-03-01 19:26:48 +0100

    code layout

--- js/tweet.js
+++ js/tweet.js
@@ -1,15 +1,15 @@
 // get selected text
 
 function getSelectionText() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-   
-    text = text.replace(/\s+/g, ' '); // remove multiple whitespaces
-    return text;
+  var text = "";
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
+  }
+ 
+  text = text.replace(/\s+/g, ' '); // remove multiple whitespaces
+  return text;
 }
 
 var mouseX;
@@ -17,58 +17,57 @@ var mouseY;
 
 // use the mouse coordinates to set button position
 jQuery(document).mousemove(function (e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY - 10;
+  mouseX = e.pageX;
+  mouseY = e.pageY - 10;
 });
 
 jQuery(document).ready(function ($) {
+  
+// put the button just after the body tag
+  $('body').prepend('<a id="tbutton"><img src="' + plicon.plugin_icon + '" width="35px"></a>');
+  
+  $('#tbutton').mouseup(function (e) {
+    $('#tbutton').hide();
+  });
+  
+// clear the button after user click
+  $('body').click(function() {
+    if( $('#tbutton').is(':visible') && getSelectionText() == "" ) {
+      $('#tbutton').hide();
+   }
+  });
+  
+// don't show the twitter button on link click  
+  $('a').click(function() {
+    $('#tbutton').hide();
+  });
+  
+// show the twitter share as a popup  
+  $('#tbutton').click(function(event) {
+    var width  = 575,
+        height = 400,
+        left   = ($(window).width()  - width)  / 2,
+        top    = ($(window).height() - height) / 2,
+        url    = this.href,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left;
+                 
+    window.open(url, 'twitter', opts);
+    return false;
+  });
     
-  // put the button just after the body tag
-    $('body').prepend('<a id="tbutton"><img src="' + plicon.plugin_icon + '" width="35px"></a>');
-    
-    $('#tbutton').mouseup(function (e) {
-        $('#tbutton').hide();
-    });
-    
-  // clear the button after user click
-    $('body').click(function() {
-      if( $('#tbutton').is(':visible') && getSelectionText() == "" ) {
-       $('#tbutton').hide();
-     }
-    });
-    
-  // don't show the twitter button on link click  
-    $('a').click(function() {
-       $('#tbutton').hide();
-    });
-    
-  // show the twitter share as a popup  
-    $('#tbutton').click(function(event) {
-        var width  = 575,
-            height = 400,
-            left   = ($(window).width()  - width)  / 2,
-            top    = ($(window).height() - height) / 2,
-            url    = this.href,
-            opts   = 'status=1' +
-                     ',width='  + width  +
-                     ',height=' + height +
-                     ',top='    + top    +
-                     ',left='   + left;
-
-        window.open(url, 'twitter', opts);
-
-        return false;
-      });
-      
-    
-    $('div,p').mouseup(function (e) {
-        if (getSelectionText() != "") {
-            $("#tbutton").attr("href", 'https://twitter.com/intent/tweet?text=' + encodeURI(getSelectionText()) + '&url=' + encodeURI(document.URL));
-            $("#tbutton").attr("target", '_blank');
-            $('#tbutton').css({
-              'top': mouseY,
-              'left': mouseX 
-            }).fadeIn('slow');
-        }
-    })
+  
+  $('div,p').mouseup(function (e) {
+    if (getSelectionText() != "") {
+      $("#tbutton").attr("href", 'https://twitter.com/intent/tweet?text=' + encodeURI(getSelectionText()) + '&url=' + encodeURI(document.URL));
+      $("#tbutton").attr("target", '_blank');
+      $('#tbutton').css({
+        'top': mouseY,
+        'left': mouseX 
+      }).fadeIn('slow');
+    }
+  })
 });
--- tweeteverything.php
+++ tweeteverything.php
@@ -11,10 +11,10 @@ Author URI: http://twitter.com/mapteo
 */
 
 function tweetable() {
-    wp_localize_script('ajax-handler', 'AjaxHandler', array('url' => admin_url('admin-ajax.php')));
-    wp_enqueue_style( 'tweet', plugins_url( '/css/tweet.css' , __FILE__ ));
-    wp_enqueue_script( 'tweet', plugins_url( '/js/tweet.js' , __FILE__ ), array( 'jquery' )); 
-    wp_localize_script('tweet', 'plicon', array( 'plugin_icon' => plugins_url( 'img/twitter-icon.png' , __FILE__ ) ));
+  wp_localize_script('ajax-handler', 'AjaxHandler', array('url' => admin_url('admin-ajax.php')));
+  wp_enqueue_style( 'tweet', plugins_url( '/css/tweet.css' , __FILE__ ));
+  wp_enqueue_script( 'tweet', plugins_url( '/js/tweet.js' , __FILE__ ), array( 'jquery' )); 
+  wp_localize_script('tweet', 'plicon', array( 'plugin_icon' => plugins_url( 'img/twitter-icon.png' , __FILE__ ) ));
 }
 
 add_action( 'wp_enqueue_scripts', 'tweetable' );
